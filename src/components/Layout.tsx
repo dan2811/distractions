4d44a53080847b45e916d5470a90657266e8b2e6
Dan Jordan <djordandrums@gmail.com>
2023-09-09 17:08:38 +0100

    basic theming

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,15 @@
-const Layout = (props: React.PropsWithChildren) => (
-  <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-    {props.children}
-  </main>
-);
+import { Exo, Noto_Sans_Display } from "next/font/google";
+const exo = Exo({ subsets: ["latin"], variable: "--font-exo" });
+const noto = Noto_Sans_Display({ subsets: ["latin"], variable: "--font-noto" });
+
+const Layout = (props: React.PropsWithChildren) => {
+  return (
+    <main
+      className={`${exo.variable} ${noto.variable} min-w-screen flex min-h-screen flex-col bg-gradient-to-b from-[rgb(0,0,0)] to-[#1e1e1e] text-white`}
+    >
+      {props.children}
+    </main>
+  );
+};
 
 export default Layout;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,21 @@
 import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          dark: "#1A1A1A",
+          accent: "#a8a07c",
+        }
+      },
+      fontFamily: {
+        headers: ['var(--font-exo)', ...fontFamily.sans],
+        body: ['var(--font-noto)', ...fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
