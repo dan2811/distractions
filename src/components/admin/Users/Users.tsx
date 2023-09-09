e1e6093ca37c1142a1e271533c59cf3081efcbb6
Daniel Jordan <80130263+dan2811@users.noreply.github.com>
2023-09-09 19:34:02 +0100

    Clean up Users.tsx

--- src/components/admin/Users/Users.tsx
+++ src/components/admin/Users/Users.tsx
@@ -41,40 +41,10 @@ export const UserEdit = () => {
   return (
     <Edit>
       <SimpleForm>
-        <TextInput source="fName" />
-        <TextInput source="lName" />
+        <TextInput source="name" />
         <TextInput source="email" />
         <TextInput source="phone" />
-        <ReferenceInput source="teacherId" reference="teacherInstrument">
-          <ReferenceArrayInput
-            source="instrumentId"
-            reference="instrument"
-            helperText={false}
-          />
-        </ReferenceInput>
-        <CustomReferenceSelect />
       </SimpleForm>
     </Edit>
   );
-};
-
-const CustomReferenceSelect = () => {
-  const record = useRecordContext();
-  interface Instrument {
-    id: string;
-    teacherId: string;
-    instrumentId: string;
-  }
-  const { data } = useGetManyReference<Instrument>("teacherInstrument", {
-    target: "teacherId",
-    id: record.id,
-  });
-  console.log(data);
-
-  const { data: instruments } = useGetMany("instrument", {
-    ids: data?.map((el) => el.instrumentId),
-  });
-
-  console.log("INSTRUMENTS: ", instruments);
-  return <></>;
 };
