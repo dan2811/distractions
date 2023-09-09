544c0ebda69a276e168c1e2d4764257e53decf61
Dan Jordan <djordandrums@gmail.com>
2023-09-09 17:41:20 +0100

    add logo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { Exo, Noto_Sans_Display } from "next/font/google";
+import { Logo } from "./Logo";
 const exo = Exo({ subsets: ["latin"], variable: "--font-exo" });
 const noto = Noto_Sans_Display({ subsets: ["latin"], variable: "--font-noto" });
 
@@ -7,6 +8,7 @@ const Layout = (props: React.PropsWithChildren) => {
     <main
       className={`${exo.variable} ${noto.variable} min-w-screen flex min-h-screen flex-col bg-gradient-to-b from-[rgb(0,0,0)] to-[#1e1e1e] text-white`}
     >
+      <Logo className="self-center" />
       {props.children}
     </main>
   );
---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import logo from "../../public/assets/images/logo_dark.png";
+
+export const Logo = (props: { className?: string }) => (
+  <Image
+    src={logo}
+    alt="The Distractions Band Logo"
+    width={175}
+    height={175}
+    className={`m-2 ${props.className ?? ""}`}
+  />
+);
