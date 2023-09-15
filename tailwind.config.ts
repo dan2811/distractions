5d5dfd0e6b56d1f4c373dbb4edd52f79f6981d97
Dan Jordan <djordandrums@gmail.com>
2023-09-15 13:19:13 +0100

    edit global theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,26 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+export const globalColors = {
+  main: {
+    dark: "#1A1A1A",
+    accent: "#a8a07c",
+  }
+};
+
+export const globalFonts = {
+  headers: ['var(--font-exo)', ...fontFamily.sans],
+  body: ['var(--font-noto)', ...fontFamily.sans],
+};
+
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        main: {
-          dark: "#1A1A1A",
-          accent: "#a8a07c",
-        }
-      },
-      fontFamily: {
-        headers: ['var(--font-exo)', ...fontFamily.sans],
-        body: ['var(--font-noto)', ...fontFamily.sans],
-      }
+      colors: globalColors,
+      fontFamily: globalFonts,
     },
   },
   plugins: [],
 } satisfies Config;
+
