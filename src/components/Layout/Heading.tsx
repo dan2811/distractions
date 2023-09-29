cb1f58db75c40cd85b61d5160046550ae485226b
Dan Jordan <djordandrums@gmail.com>
2023-09-29 11:45:45 +0100

    sticky headers

--- src/components/Layout/Heading.tsx
+++ src/components/Layout/Heading.tsx
@@ -1,5 +1,5 @@
 export const Heading = (props: React.PropsWithChildren) => (
-  <div className="flex w-2/3 flex-row items-center justify-end gap-2 rounded-r-md bg-main-accent py-2 pr-2 font-headers md:max-w-sm">
+  <div className="sticky top-14 flex w-2/3 flex-row items-center justify-end gap-2 rounded-r-md bg-main-accent py-2 pr-2 font-headers md:max-w-sm">
     <hr className="flex-grow" />
     {props.children}
   </div>
