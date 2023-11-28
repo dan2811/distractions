21af0189d26c04fb28cc0078534b07a6cea1c5c2
Dan Jordan <djordandrums@gmail.com>
2023-11-28 17:19:44 +0000

    update contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Heading } from "~/components/Layout/Heading";
 import Layout from "~/components/Layout/Layout";
+import Joseph from "../../../public/assets/images/joseph_malik.jpeg";
+import Image from "next/image";
 
 const contact = () => {
   return (
@@ -11,9 +13,22 @@ const contact = () => {
       <Heading>
         <h2>Contact</h2>
       </Heading>
-      <span>Joseph Malik</span>
-      <span>Band Manager</span>
-      <a href="tel:07977991235">07977991235</a>
+      <div className="flex w-full self-center p-4">
+        <div className="flex w-full content-between rounded-lg bg-gradient-to-tl from-gray-900/40 to-gray-300/50 p-6 bg-blend-darken shadow-inner shadow-gray-500 backdrop-blur-md">
+          <Image
+            src={Joseph}
+            alt="Joseph Malik"
+            width={130}
+            height={130}
+            className="w-1/2 rounded-full"
+          />
+          <div className="flex w-full flex-col justify-center text-center">
+            <span>Joseph Malik</span>
+            <span>Band Manager</span>
+            <a href="tel:07977991235">07977991235</a>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
