08bb3afeb0c741da23e9038860a06c3e0ce5a232
Dan Jordan <djordandrums@gmail.com>
2024-04-03 16:54:55 +0100

    update cancel events and API prep for stats

--- src/server/api/routers/jobs.ts
+++ src/server/api/routers/jobs.ts
@@ -1,55 +1,79 @@
 import { z } from "zod";
 
-import {
-    createTRPCRouter, protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { prisma } from "~/server/db";
 
 export const jobRouter = createTRPCRouter({
-    getMyJobs: protectedProcedure.input(z.object({
-        filter: z.object({
+  getMyJobs: protectedProcedure
+    .input(
+      z
+        .object({
+          filter: z.object({
             status: z.string().optional(),
             date_gte: z.string().optional(),
+          }),
+          includeEvents: z.boolean().optional().default(false),
         })
-    }).optional()).query(({ ctx, input }) => {
-        return prisma.job.findMany({
-            include: {
-                Instruments: true,
+        .optional(),
+    )
+    .query(({ ctx, input }) => {
+      const include: { Instruments: boolean; event?: object } = {
+        Instruments: true,
+      };
+      if (input?.includeEvents) {
+        include.event = {
+          select: {
+            id: true,
+            date: true,
+            name: true,
+            location: true,
+          },
+        };
+      }
+      return prisma.job.findMany({
+        include,
+        where: {
+          musicianId: ctx.session.user.id,
+          status: input?.filter?.status,
+          event: {
+            date: {
+              gte: input?.filter?.date_gte,
             },
-            where: {
-                musicianId: ctx.session.user.id,
-                status: input?.filter?.status,
-                event: {
-                    date: {
-                        gte: input?.filter?.date_gte
-                    }
-                }
-            }
-        });
+          },
+        },
+      });
     }),
-    getOne: protectedProcedure.input(z.object({
+  getOne: protectedProcedure
+    .input(
+      z.object({
         eventId: z.string(),
-    })).query(({ input }) => {
-        return prisma.job.findFirstOrThrow({
-            where: {
-                eventId: input.eventId,
-            },
-            include: {
-                Instruments: true,
-            }
-        });
+      }),
+    )
+    .query(({ input }) => {
+      return prisma.job.findFirstOrThrow({
+        where: {
+          eventId: input.eventId,
+        },
+        include: {
+          Instruments: true,
+        },
+      });
     }),
-    updateOne: protectedProcedure.input(z.object({
+  updateOne: protectedProcedure
+    .input(
+      z.object({
         jobId: z.string(),
-        status: z.string()
-    })).mutation(({ input }) => {
-        return prisma.job.update({
-            where: {
-                id: input.jobId
-            },
-            data: {
-                status: input.status
-            }
-        });
-    })
+        status: z.string(),
+      }),
+    )
+    .mutation(({ input }) => {
+      return prisma.job.update({
+        where: {
+          id: input.jobId,
+        },
+        data: {
+          status: input.status,
+        },
+      });
+    }),
 });
